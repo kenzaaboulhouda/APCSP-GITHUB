b8a85250406eea36d9a50460a13ff16daddcf0ed
kenza aboulhouda <796606@seq.org>
2018-10-24 00:05:17 -0700

    changes to word.txt and sketch.js

--- WordSort.1022/sketch.js
+++ WordSort.1022/sketch.js
@@ -3,3 +3,24 @@
 **  Kenza Aboulhouda
 **  October 4, 2018
 */
+var txt;
+
+function preload(){
+  // p5 has a function named "loadStrings"
+  txt = loadStrings("words.txt");
+}
+
+function setup() {
+
+}
+
+
+
+
+for(var i = nums.length - 1; i >= 1; i-- ){
+    for(var j = 0; j < nums.length; j++ ){
+
+}
+
+
+}
