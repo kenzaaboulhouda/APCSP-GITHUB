9b33f6b5d1d49498b2c2a634548b746bd35cf024
kenza aboulhouda <796606@seq.org>
2018-11-01 13:38:23 -0700

    json update

--- 1101JSOn/sketch.js
+++ 1101JSOn/sketch.js
@@ -1,11 +1,13 @@
 //  Global variables
-var bars = [];
+var data = [];
+
+function preLoad(){
+  data = loadJSON("earthquakes.geoson");
+  console.log(data);
+}
 
 // put setup code here
 function setup() {
-  var cnv = createCanvas(1200, 800);
-  cnv.position((windowWidth-width)/2, 30);
-  background(20,20,20);
-  bars = new Bars(createVector(width/2,height/2),
-createVector(.5,-.8),25, color(255,0,0));
+  noCanvas();
+  console.log(data.description)
 }
