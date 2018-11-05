e31c02f5e7d02a1bb2d73145d557d8cb48d71c53
kenza aboulhouda <796606@seq.org>
2018-11-05 08:46:29 -0800

    weather update

--- 1101JSOn/sketch.js
+++ 1101JSOn/sketch.js
@@ -2,12 +2,26 @@
 var data = [];
 
 function preLoad(){
-  data = loadJSON("earthquakes.geoson");
+  data = loadJSON("earthquakes.json.geoson");
   console.log(data);
 }
-
+function bubbleSort(){
+  for (var i = (data.length - 1); i >= 0; i--){
+    for (var j = 0; j < i; j++){
+      if(date[j].high < data[j+1].high){
+        var temp = data[j]
+        data[j] = data[i+1]
+        data[j+1] = temp
+      }
+    }
+  }
+}
 // put setup code here
 function setup() {
   noCanvas();
-  console.log(data.description)
+  console.log(data.forecast)
+}
+
+fucntion draw(){
+  
 }
