c7ad4706e4bc2ef108ed18f69c843ba544e6b744
kenza aboulhouda <796606@seq.org>
2018-11-07 08:37:44 -0800

    update

--- 1101JSOn/sketch.js
+++ 1101JSOn/sketch.js
@@ -2,13 +2,13 @@
 var data = [];
 
 function preLoad(){
-  data = loadJSON("earthquakes.json.geoson");
+  data = loadJSON("weatherdata.json");
   console.log(data);
 }
 function bubbleSort(){
   for (var i = (data.length - 1); i >= 0; i--){
     for (var j = 0; j < i; j++){
-      if(date[j].high < data[j+1].high){
+      if(date[j].forecast.high < data[j+1].forecast.high){
         var temp = data[j]
         data[j] = data[i+1]
         data[j+1] = temp
@@ -23,5 +23,5 @@ function setup() {
 }
 
 fucntion draw(){
-  
+
 }
